ac60348b7e85924ee0057abb324b0cff9cbb43b3
Alexandre Figueiredo <17xande@gmail.com>
2022-12-14 11:58:28 +0200

    almost working with recursion

--- day07/main.go
+++ day07/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -10,17 +11,93 @@ type file struct {
 	name     string
 	size     int
 	isDir    bool
-	children []file
+	children map[string]*file
 }
 
 func main() {
 	lines := input()
+	root := parse(lines)
+	root.print(0)
 
-	fmt.Println(lines)
+	// fmt.Printf("\nResult: %+v\n", parse(lines))
+
+}
+
+func (f *file) print(indent int) {
+	if f.isDir {
+		fmt.Printf("%s-d %s %d\n", strings.Repeat(" ", indent), f.name, f.size)
+
+		for _, c := range f.children {
+			c.print(indent + 2)
+		}
+	} else {
+		fmt.Printf("%s-f %s %d\n", strings.Repeat(" ", indent), f.name, f.size)
+	}
+}
+
+func (f *file) sumSize() {
+	if !f.isDir {
+		return
+	}
+
+	for _, c := range f.children {
+		f.size += c.size
+	}
+}
+
+func parse(lines []string) *file {
+	root := file{
+		name:     "/",
+		isDir:    true,
+		children: make(map[string]*file),
+	}
+
+	root.ls(lines, 2)
+
+	return &root
+}
+
+func (f *file) ls(lines []string, currentLine int) {
+	for i := currentLine; i < len(lines); i++ {
+		l := lines[i]
+		if l == "$ cd .." {
+			return
+		}
+
+		var child file
+		if l[:4] == "$ cd" {
+			f = f.children[l[5:]]
+			continue
+		}
+
+		if l[:4] == "$ ls" {
+			f.ls(lines, i+1)
+			break
+		}
+
+		if l[:3] == "dir" {
+			child = file{
+				name:     l[4:],
+				isDir:    true,
+				children: make(map[string]*file),
+			}
+		} else {
+			s := strings.Split(l, " ")
+			size, _ := strconv.Atoi(s[0])
+			child = file{
+				name:  s[1],
+				isDir: false,
+				size:  size,
+			}
+		}
+
+		f.children[child.name] = &child
+	}
+	f.sumSize()
 }
 
 func input() []string {
-	bytes, err := os.ReadFile("input.txt")
+	bytes, err := os.ReadFile("test.txt")
 	if err != nil {
 		panic(err)
 	}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/17xande/adventofcode
+
+go 1.19
